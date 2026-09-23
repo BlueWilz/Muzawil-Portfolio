ecb698b95d2a5e52175450c84c3c4efc40c19614
BlueWilz <zawil.polem@gmail.com>
2026-09-23 16:53:06 +0800

    Change default basePath to root directory

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ if (Number.isNaN(port) || port <= 0) {
   throw new Error(`Invalid PORT value: "${rawPort}"`);
 }
 
-const basePath = process.env.BASE_PATH || '/Muzawil-Portfolio/';
+const basePath = process.env.BASE_PATH || '/';
 
 if (!basePath) {
   throw new Error(
